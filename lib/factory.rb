a179b3d5f8d128d33935173bc0c7182f47d7acc4
Tyler Andrews <tandrews@skeinc.com>
2016-01-04 13:05:19 -0400

    Made register product work with only passing the name of the class (non-string)

--- lib/factory.rb
+++ lib/factory.rb
@@ -1,6 +1,12 @@
 module Factory
+  @registered_products = {}
+
+  # Usage:
+  # Factory.registered_product('id', Product)
+  #   Where Product is the name of the class, eg:
+  #     Factory.registered_product('id', String)
   def self.registered_product(product_id, product)
-    @registered_products[product_id] = product
+    @registered_products[product_id] = product.new.class
   end
 
   def self.create_product(product_id)
