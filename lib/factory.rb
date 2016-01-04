7d6877364db75f7ad3d74772df1e0836fa90dd6d
Tyler Andrews <tandrews@skeinc.com>
2016-01-04 15:52:06 -0400

    Got a universal factory to work

--- lib/factory.rb
+++ lib/factory.rb
@@ -1,16 +1,11 @@
 module Factory
   @registered_products = {}
 
-  def self.registered_product(product_id, product)
+  def self.register_product(product_id, product)
     @registered_products[product_id] = product.new.class
   end
 
   def self.create_product(product_id, *args)
-    if args[1].nil?
-      @registered_products.fetch(product_id).new
-    else
-      new_args = args[1..args.length]
-      @registered_products.fetch(product_id).new *new_args
-    end
+      @registered_products.fetch(product_id).new(*args)
   end
 end
