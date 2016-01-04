815751b70bdbf7514908efa6ec29f1cffaf3003e
Tyler Andrews <tandrews@skeinc.com>
2016-01-04 13:45:38 -0400

    Made methods conform to new Product specs

--- lib/factory.rb
+++ lib/factory.rb
@@ -6,10 +6,10 @@ module Factory
   #   Where Product is the name of the class, eg:
   #     Factory.registered_product('id', String)
   def self.registered_product(product_id, product)
-    @registered_products[product_id] = product.new.class
+    @registered_products[product_id] = product.new('').class
   end
 
   def self.create_product(product_id)
-    @registered_products.fetch(product_id).new
+    @registered_products.fetch(product_id).new product_id
   end
 end
